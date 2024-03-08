175bc2243add71b8529c7e937d5ec6bd3c68affb
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2024-03-08 13:16:08 +0700

    5

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/404.module.css";
+
+const Custom404 = () => {
+  return (
+    <div className={styles.error}>
+      <div>404 | Page not found</div>
+    </div>
+  );
+};
+
+export default Custom404;
