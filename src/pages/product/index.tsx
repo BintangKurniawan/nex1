c3c680e805d5349b9283162da8ec6bccbb296727
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2024-03-13 10:03:30 +0700

    7

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { fetcher } from "@/lib/swr/fetcher";
+import ProductView from "@/views/product";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  variant: string;
-};
+import useSWR from "swr";
+
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
@@ -17,23 +15,18 @@ const ProductPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetch("/api/product")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/product")
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data.data));
+  // }, []);
+
+  const { data, error, isLoading } = useSWR("/api/product", fetcher);
 
   return (
-    <>
-      <h1>Product page</h1>
-      {products.map((product: productType) => (
-        <div key={product.id}>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
-          <p>{product.variant}</p>
-        </div>
-      ))}
-    </>
+    <div>
+      <ProductView products={isLoading ? [] : data.data} />
+    </div>
   );
 };
 
